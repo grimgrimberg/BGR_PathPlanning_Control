6e296a586aa3e14eee2406266e1cbcefe6a0a47f
Ori Komemi <orikom123@gmail.com>
2025-05-23 11:34:34 +0300

    update lidar provider

--- providers/lidar/cones.py
+++ providers/lidar/cones.py
@@ -19,20 +19,27 @@ class LidarConeProvider:
     def __init__(self):
         self.lidar_mod = LidarModule()
         log.debug(f"Provider {self.__class__.__name__} initialized successfully")
-    
+        self.prev = {}
     def start(self):     pass
     def stop(self):      pass
 
     def read(self) -> Dict[str, np.ndarray]:
 
         detections = self.lidar_mod.scan_detect()
-        lidar_cones = np.array([detections[i][:2] for i in sorted(detections.keys())])
-        
+        log.info(f"Detected {len(detections)} cones")
+        if len(detections) < 5:
+            return self.prev
+        lidar_cones = np.array([detections[i][:2] for i in sorted(detections.keys())]) / 100.0
+        print(lidar_cones.shape)
         cones_by_type = [np.zeros((0, 2)) for _ in range(5)]
 
-        cones_by_type[ConeTypes.UNKNOWN] = np.array(lidar_cones)/100
+        cones_by_type[ConeTypes.UNKNOWN] = np.array(lidar_cones)
+        log.info(f"Unknown cones: {cones_by_type[ConeTypes.UNKNOWN].shape}")
         cones_lidar = np.array(lidar_cones)
-        return {
-            'cones':(cones_by_type, 0, 0),
-            'cones_lidar':cones_lidar    
+        car_position = np.array([0,0])
+        car_direction = np.array([np.cos(0),np.sin(0)])
+        self.prev = {
+            'cones':(cones_by_type, car_position, car_direction),
+            'cones_lidar': cones_lidar    
             }
+        return self.prev
